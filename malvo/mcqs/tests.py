f877debfea962d53388ea5fc95be127f28572b65
Shivam Mamgain <shivammamgain47@gmail.com>
2016-02-08 22:12:35 +0530

    Add tests for mcq models

--- malvo/mcqs/tests.py
+++ malvo/mcqs/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionMethodTests(TestCase):
+
+    def test_default_question_no_for_new_question(self):
+        """
+        incre_question_count() should return the number of questions
+        incremented by one
+        """
+        number_of_questions = Question.objects.count()
+        if number_of_questions == None:
+            count = 0
+        else:
+            count = number_of_questions
+        incremented_count = count + 1
+        self.assertEqual(Question.incre_question_count(), incremented_count)
