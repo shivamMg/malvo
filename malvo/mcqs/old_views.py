3425dbdfc5b3ed6e1f620bc4818452e3e15af236
Shivam Mamgain <shivammamgain47@gmail.com>
2016-02-20 23:00:45 +0530

    Separate old mcqs views

---
+++ malvo/mcqs/old_views.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+from .models import Question, Choice
+
+
+class McqView(LoginRequiredMixin, generic.DetailView):
+    model = Question
+    template_name = 'mcqs/mcq.html'
+
+
+@login_required
+def answer(request, question_no):
+    question_count = Question.objects.count()
+    question = get_object_or_404(Question, question_no=question_no)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        print(selected_choice)
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'mcqs/mcq.html', {
+            'question': question,
+            'error_message': 'No option selected',
+        })
+    else:
+        # if it's the last question, redirect to index page
+        # else redirect to next question
+        if question.question_no == question_count:
+            return HttpResponseRedirect(reverse('mcqs:index'))
+        else:
+            return HttpResponseRedirect(reverse('mcqs:mcq', args=(
+                question.question_no + 1,
+            )))
--- malvo/mcqs/views.py
+++ malvo/mcqs/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render#, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
-# from django.contrib.auth.decorators import login_required
 
-from .models import Question#, Choice
+from .models import Question
 from .dump_mcqs import dump_mcqs_to_file
 from teams.models import Team, TeamMcqAnswer
 
@@ -16,11 +15,6 @@ class IndexView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Question.objects.order_by('question_no')
-
-# Multipage MCQ view
-# class McqView(LoginRequiredMixin, generic.DetailView):
-#     model = Question
-#     template_name = 'mcqs/mcq.html'
 
 
 # @login_required
@@ -46,26 +40,3 @@ def answer(request):
         return HttpResponseRedirect(reverse('mcqs:index'))
     else:
         return HttpResponseRedirect(reverse('mcqs:index'))
-
-
-# @login_required
-# def answer(request, question_no):
-#     question_count = Question.objects.count()
-#     question = get_object_or_404(Question, question_no=question_no)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-#         print(selected_choice)
-#     except (KeyError, Choice.DoesNotExist):
-#         return render(request, 'mcqs/mcq.html', {
-#             'question': question,
-#             'error_message': 'No option selected',
-#         })
-#     else:
-#         # if it's the last question, redirect to index page
-#         # else redirect to next question
-#         if question.question_no == question_count:
-#             return HttpResponseRedirect(reverse('mcqs:index'))
-#         else:
-#             return HttpResponseRedirect(reverse('mcqs:mcq', args=(
-#                 question.question_no + 1,
-#             )))
